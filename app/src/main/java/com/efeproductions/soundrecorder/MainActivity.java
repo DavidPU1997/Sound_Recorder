33a090055c0b355ce2eedba87359d0dc97c9e643
davidpu97 <david.pu97@gmail.com>
2019-04-24 20:24:11 +0200

    - Added Playback UI  - Created links between activities

--- app/src/main/java/com/efeproductions/soundrecorder/MainActivity.java
+++ app/src/main/java/com/efeproductions/soundrecorder/MainActivity.java
@@ -1,10 +1,17 @@
 package com.efeproductions.soundrecorder;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void callPlayback(View v){
+        Intent playback = new Intent(this, PlaybackActivity.class);
+        startActivity(playback);
     }
 }
