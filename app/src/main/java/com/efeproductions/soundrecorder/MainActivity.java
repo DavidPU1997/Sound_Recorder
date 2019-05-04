c4d132d9c6ee035a0fe8ea868a520a44552f4820
mb7008 <bogataj.marjan.97@gmail.com>
2019-05-04 12:25:58 +0200

    Recording narjen, sam skos kr neki crasha in nevem zakaj

--- app/src/main/java/com/efeproductions/soundrecorder/MainActivity.java
+++ app/src/main/java/com/efeproductions/soundrecorder/MainActivity.java
@@ -1,83 +1,25 @@
 package com.efeproductions.soundrecorder;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
-
-    //declare variables
-    Button btnRecord, btnStopRecord;
-    String pathSave = "";
-    MediaRecorder mediaRecorder;
-    MediaPlayer mediaPlayer;
-
-    final int REQUEST_PERMISSION_CODE = 1000;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //from Android M , you need request Run time permission
-        if(checkPermissionFromDevice()){
-
-        }
-        else{
-            requestPermissions();
-        }
-
-    }
-// penis
-    private void requestPermissions() {
-        ActivityCompat.requestPermissions(this, new String[]{
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.RECORD_AUDIO
-        }, REQUEST_PERMISSION_CODE);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch(requestCode){
-            case REQUEST_PERMISSION_CODE:
-                {
-                    if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
-                        Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-                    else
-                        Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-                }
-                break;
-        }
-    }
-
-    private boolean checkPermissionFromDevice() {
-        int write_external_storage_result = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        int record_audio_result = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        return write_external_storage_result == PackageManager.PERMISSION_GRANTED &&
-                record_audio_result == PackageManager.PERMISSION_GRANTED;
-    }
-
-    public void callPlayback(View v){
+    public void callPlayback(View v) {
         Intent playback = new Intent(this, PlaybackActivity.class);
         startActivity(playback);
     }
+
+    public void callRecording(View v) {
+        Intent record = new Intent(this, RecordingActivity.class);
+        startActivity(record);
+    }
 }
+
